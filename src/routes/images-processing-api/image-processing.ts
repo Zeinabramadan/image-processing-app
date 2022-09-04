850e73941cfe9e3f1338ed1583a90abc8f5411e1
Zeinab Ramadan <zeinabramadan021@gmail.com>
2022-09-04 14:26:02 +0200

    feat: cache the image if exists

--- src/routes/images-processing-api/image-processing.ts
+++ src/routes/images-processing-api/image-processing.ts
@@ -17,6 +17,7 @@ imageProcessingRoutes.get('/images', async (req: Request, res: Response) => {
   const isImageNameExist = IMAGES_NAMES.includes(name)
   const location = path.resolve(`./assets/images/${name}.jpg`)
   const thumbnailsLocation = path.resolve('./assets/thumbnails')
+  const currentImage = thumbnailsLocation + `/${name}x${width}x${height}.png`
 
   // Checks and display an error if the name query is not provided
   if (name === undefined) {
@@ -33,11 +34,16 @@ imageProcessingRoutes.get('/images', async (req: Request, res: Response) => {
     return res.status(404).send(ERROR_404_NOT_FOUND)
   }
 
-  await resizeImages(location, name, width, height)
-
-  res
-    .status(200)
-    .sendFile(thumbnailsLocation + `/${name}x${width}x${height}.png`)
+  // Check if the resized Image exists so send it
+  // to the user instead of re-resizing it again
+  if (existsSync(currentImage)) {
+    res.status(200).sendFile(currentImage)
+  } else {
+    await resizeImages(location, name, width, height)
+    res
+      .status(200)
+      .sendFile(thumbnailsLocation + `/${name}x${width}x${height}.png`)
+  }
 })
 
 export default imageProcessingRoutes
